96c097b458888a3741dc58dda12b1012ed4cb78f
ShreyasMalakarjunPatil <sm_patil@gatech.edu>
2021-06-09 17:16:48 -0400

    Updated kernel PHEW main

--- Experiments/Ablations/Kernel_PHEW.py
+++ Experiments/Ablations/Kernel_PHEW.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from Utils import load
 from Utils import train
-from Prune import phew_utils
+from Prune import kernel_phew_utils
 import copy
 import pickle as pkl
 from Prune import Utils
@@ -16,12 +16,12 @@ def run(args):
     test_loader = load.dataloader(args.dataset, args.train_batch_size, False, args.workers)
 
     model = load.model(args.model, args.dataset)(input_shape, num_classes).to(dev)
-    prob, reverse_prob, kernel_prob = phew_utils.generate_probability(model, verbose = True)
+    prob, reverse_prob = kernel_phew_utils.generate_probability(model, verbose = True)
 
     net = copy.deepcopy(model)
     for p in net.parameters():
         p.data.fill_(0)
-    weight_masks, bias_masks = phew_utils.generate_masks(net)
+    weight_masks, bias_masks = kernel_phew_utils.generate_masks(net)
 
     for i in range(len(args.prune_perc)):
         print(args.experiment, str(args.prune_perc[len(args.prune_perc) - i - 1]), str(args.model), str(args.dataset))
@@ -31,7 +31,7 @@ def run(args):
         loss = nn.CrossEntropyLoss()
         opt, opt_kwargs = load.optimizer(args.optimizer)
 
-        weight_masks, bias_masks = phew_utils.phew_masks(model, prune_perc, prob, reverse_prob, kernel_prob,weight_masks,bias_masks,verbose=True,kernel_conserved=True)
+        weight_masks, bias_masks = kernel_phew_utils.phew_masks(model, prune_perc, prob, reverse_prob, weight_masks,bias_masks,verbose=True,kernel_conserved=True)
         sparse_model.set_masks(weight_masks, bias_masks)
         sparse_model.to(dev)
 
